24b9774b9283f97f1ae31dec07ebeba02f8e3bdc
Nathan Mailumo <nathandrock008@gmail.com>
2026-08-13 12:07:16 +0100

    changed the styling of the pages

--- devdashboard/src/components/navbar.jsx
+++ devdashboard/src/components/navbar.jsx
@@ -3,19 +3,19 @@ import "./navbar.css";
 
 export function NavBar(){
     return(
-        <div className="sidebar">
-        <a className="logo" href="">
-          NAME.COM
-        </a>
-        <h3><Link to="/">Dashboard</Link></h3>
-        <div className="menu">
-          <ul>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/favourites">Favourites</Link></li>
-            <li><Link to="/trash">Trash</Link></li>
-            <li><Link to="/settings">Settings</Link></li>
-          </ul>
-        </div>
-      </div>
+        <nav className="navbar">
+          <a className="logo" href="/">
+            NAME.COM
+          </a>
+          <div className="menu">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/favourites">Favorites</Link></li>
+              <li><Link to="/trash">Trash</Link></li>
+              <li><Link to="/settings">Settings</Link></li>
+            </ul>
+          </div>
+        </nav>
     )
 }
